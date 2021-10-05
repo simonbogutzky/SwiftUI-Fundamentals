4bfc224664429d7545ab85316f3a34bf0839c7ab
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-05 09:19:07 +0200

    HStack of Days & Custom Color

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(
-                gradient: Gradient(colors: [.blue, .white]),
+                gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
@@ -29,6 +29,28 @@ struct ContentView: View {
                     Text("76°")
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
+                }.padding(.bottom, 40)
+                HStack(spacing: 20) {
+                    WeatherDayView(
+                        weekOfDay: "Tue",
+                        imageName: "cloud.sun.fill",
+                        temperature: 74)
+                    WeatherDayView(
+                        weekOfDay: "Wed",
+                        imageName: "sun.max.fill",
+                        temperature: 88)
+                    WeatherDayView(
+                        weekOfDay: "Thu",
+                        imageName: "wind.snow",
+                        temperature: 55)
+                    WeatherDayView(
+                        weekOfDay: "Fri",
+                        imageName: "sunset.fill",
+                        temperature: 60)
+                    WeatherDayView(
+                        weekOfDay: "Sat",
+                        imageName: "snow",
+                        temperature: 25)
                 }
                 Spacer()
             }
@@ -39,5 +61,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var weekOfDay: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Text(weekOfDay)
+                .font(.system(size: 16, weight: .medium))
+                .textCase(.uppercase)
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original).resizable()
+                .aspectRatio( contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
