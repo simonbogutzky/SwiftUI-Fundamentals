04e296b073254a406cfd9ddf4b255825cc1fbbd1
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-11 16:25:13 +0200

    Refactor, Project Organization

--- Apple-Frameworks/Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -11,16 +11,10 @@ struct FrameworkGridView: View {
     
     @StateObject var viewModel: FrameworkGridViewModel = FrameworkGridViewModel()
     
-    let coloums: [GridItem] = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-    ]
-    
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: coloums) {
+                LazyVGrid(columns: viewModel.coloums) {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -41,23 +35,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
             .preferredColorScheme(.dark)
-    }
-}
-
-struct FrameworkTitleView: View {
-    let framework: Framework
-    
-    var body: some View {
-        VStack {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
     }
 }
