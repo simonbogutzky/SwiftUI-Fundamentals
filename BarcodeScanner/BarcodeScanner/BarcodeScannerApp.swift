64d6a5d4cb6e18b48bcf89753b02b32d531dab6e
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-12 15:51:10 +0200

    Barcode Scanner - UIKit Integration Intro & UI Setup

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Simon Bogutzky on 12.10.21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
