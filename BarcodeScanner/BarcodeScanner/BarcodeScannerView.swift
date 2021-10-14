326a4215430f17c6e01823dd532691a0b3262178
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-14 15:38:33 +0200

    Errors and Alerts

--- BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -6,15 +6,31 @@
 //
 
 import SwiftUI
+import CoreMedia
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input", message: "Something is wrong with the camera. We are unable to capture the input", dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scanned Type", message: "The value scanned is not valid. This app scans EAN-8 and EAN-13", dismissButton: .default(Text("OK")))
+}
 
 struct BarcodeScannerView: View {
     
     @State private var scannedCode: String = ""
     
+    @State private var alertItem: AlertItem?
+    
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode)
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer().frame(height: 60)
@@ -29,6 +45,13 @@ struct BarcodeScannerView: View {
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(
+                    title: Text(alertItem.title),
+                    message: Text(alertItem.message),
+                    dismissButton: alertItem.dismissButton
+                )
+            }
         }
     }
 }
--- BarcodeScanner/BarcodeScanner/ScannerView.swift
+++ BarcodeScanner/BarcodeScanner/ScannerView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ScannerView: UIViewControllerRepresentable {
     
     @Binding var scannedCode: String
+    @Binding var alertItem: AlertItem?
     
     func makeUIViewController(context: Context) -> ScannerVC {
         ScannerVC(scannerDelegate: context.coordinator)
@@ -34,13 +35,12 @@ struct ScannerView: UIViewControllerRepresentable {
         }
         
         func didSurfaceError(error: CameraError) {
-            print(error.rawValue)
+            switch error {
+            case .invalidDeviceInput:
+                scannerView.alertItem = AlertContext.invalidDeviceInput
+            case .invalidScannedValue:
+                scannerView.alertItem = AlertContext.invalidScannedType
+            }
         }
-    }
-}
-
-struct ScannerView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScannerView(scannedCode: .constant("12345"))
     }
 }
