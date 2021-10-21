683ec15f8d2e6d91f176d2ab573280de7b5ad00b
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-21 11:53:26 +0200

    NetworkManager

---
+++ Appertizers/Appertizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appertizers
+//
+//  Created by Simon Bogutzky on 21.10.21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableComplete
+}
